78039393758cb856eb5e200272c1127fd8531524
BatyLeo <leo.baty@eleves.enpc.fr>
2018-12-05 13:46:48 +0100

    inversion des cascades

--- back/game/models.py
+++ back/game/models.py
@@ -3,62 +3,6 @@ from django.db import models
 import game.game_settings as constant
 
 
-class Resources(models.Model):
-    """
-    Resources model
-
-    Fields:
-        um (int):
-        hydrocarbons (int):
-        pollution (int):
-    """
-    um = models.IntegerField(default=constant.UM_INITIAL_STOCK)
-    hydrocarbons = models.IntegerField(default=constant.HYDROCARBONS_INITIAL_STOCK)
-    # pollution = models.IntegerField(default=constant.POLLUTION_INITIAL_STOCK)
-
-
-class Production(models.Model):
-    """
-    Production model
-
-    Fields:
-        um (int):
-        hydrocarbons (int):
-        food (int):
-        electricity (int):
-        pollution (int):
-        waste (int):
-    """
-    um = models.IntegerField(default=constant.UM_INITIAL_PRODUCTION)
-    hydrocarbons = models.IntegerField(default=constant.HYDROCARBONS_INITIAL_PRODUCTION)
-    food = models.IntegerField(default=constant.FOOD_INITIAL_PRODUCTION)
-    electricity = models.IntegerField(default=constant.ELECTRICITY_INITIAL_PRODUCTION)
-    pollution = models.IntegerField(default=constant.POLLUTION_INITIAL_PRODUCTION)
-    waste = models.IntegerField(default=constant.WASTE_INITIAL_PRODUCTION)
-
-
-class States(models.Model):
-    """
-    States wheel model
-
-    Fields:
-        economical (int): economical level between 0 and constant.MAX_STATE_VALUE
-                            initial value : constant.ECONOMICAL_INITIAL_VALUE
-        social (int): social level between 0 and constant.MAX_STATE_VALUE
-                        initial value : constant.SOCIAL_INITIAL_VALUE
-        environmental (int): environmental level between 0 and constant.MAX_STATE_VALUE
-                        initial value : constant.ENVIRONMENTAL_INITIAL_VALUE
-    """
-    economical = models.IntegerField(default=constant.ECONOMICAL_INITIAL_VALUE)
-    social = models.IntegerField(default=constant.SOCIAL_INITIAL_VALUE)
-    environmental = models.IntegerField(default=constant.ENVIRONMENTAL_INITIAL_VALUE)
-
-    def green_income(self):
-        """ Return environment regeneration income corresponding to the environmental level """
-        self.environmental += self.environmental // constant.ENVIRONMENTAL_REGENERATION_LEVEL
-        self.environmental = min(self.environmental, 100)
-
-
 class Game(models.Model):
     """
     Game model
@@ -152,9 +96,6 @@ class Player(models.Model):
     """
     game = models.ForeignKey(Game, on_delete=models.CASCADE, related_name="players")
     name = models.CharField(max_length=100, default="Anne O'NYME")
-    resources = models.OneToOneField(Resources, on_delete=models.CASCADE)
-    production = models.OneToOneField(Production, on_delete=models.CASCADE)
-    states = models.OneToOneField(States, on_delete=models.CASCADE)
 
     # Encore a traiter vv
     technologies = models.IntegerField(default=0)
@@ -180,6 +121,67 @@ class Player(models.Model):
         return self.states.green_income()
 
 
+class Resources(models.Model):
+    """
+    Resources model
+
+    Fields:
+        um (int):
+        hydrocarbons (int):
+        pollution (int):
+    """
+    player = models.OneToOneField(Player, on_delete=models.CASCADE)
+
+    um = models.IntegerField(default=constant.UM_INITIAL_STOCK)
+    hydrocarbons = models.IntegerField(default=constant.HYDROCARBONS_INITIAL_STOCK)
+    # pollution = models.IntegerField(default=constant.POLLUTION_INITIAL_STOCK)
+
+
+class Production(models.Model):
+    """
+    Production model
+
+    Fields:
+        um (int):
+        hydrocarbons (int):
+        food (int):
+        electricity (int):
+        pollution (int):
+        waste (int):
+    """
+    player = models.OneToOneField(Player, on_delete=models.CASCADE)
+
+    um = models.IntegerField(default=constant.UM_INITIAL_PRODUCTION)
+    hydrocarbons = models.IntegerField(default=constant.HYDROCARBONS_INITIAL_PRODUCTION)
+    food = models.IntegerField(default=constant.FOOD_INITIAL_PRODUCTION)
+    electricity = models.IntegerField(default=constant.ELECTRICITY_INITIAL_PRODUCTION)
+    pollution = models.IntegerField(default=constant.POLLUTION_INITIAL_PRODUCTION)
+    waste = models.IntegerField(default=constant.WASTE_INITIAL_PRODUCTION)
+
+
+class States(models.Model):
+    """
+    States wheel model
+
+    Fields:
+        economical (int): economical level between 0 and constant.MAX_STATE_VALUE
+                            initial value : constant.ECONOMICAL_INITIAL_VALUE
+        social (int): social level between 0 and constant.MAX_STATE_VALUE
+                        initial value : constant.SOCIAL_INITIAL_VALUE
+        environmental (int): environmental level between 0 and constant.MAX_STATE_VALUE
+                        initial value : constant.ENVIRONMENTAL_INITIAL_VALUE
+    """
+    player = models.OneToOneField(Player, on_delete=models.CASCADE)
+
+    economical = models.IntegerField(default=constant.ECONOMICAL_INITIAL_VALUE)
+    social = models.IntegerField(default=constant.SOCIAL_INITIAL_VALUE)
+    environmental = models.IntegerField(default=constant.ENVIRONMENTAL_INITIAL_VALUE)
+
+    def green_income(self):
+        """ Return environment regeneration income corresponding to the environmental level """
+        return self.environmental // constant.ENVIRONMENTAL_REGENERATION_LEVEL
+
+
 class HydrocarbonSupplyPile(models.Model):
     """
     Pile d'hydrocarbures
@@ -200,7 +202,7 @@ class HydrocarbonSupplyPile(models.Model):
 
     def decrease(self, diminution):
         """ Diminue le stock de diminution """
-        self.stock_amount -= diminution
+        self.stock_amount -= diminution * self.multiplier
         self.save()
 
     def setTo(self, value):
