f5593bd0ab3c23f21e8279e0f47fe0b297b641bf
BatyLeo <leo.baty@eleves.enpc.fr>
2018-12-14 15:37:55 +0100

    tests folder created

---
+++ back/game/tests/test_player.py
@@ -0,0 +1,6 @@
+from django.test import TestCase
+
+
+class PlayerTest(TestCase):
+    def test_income_works(self):
+        self.assertTrue(True)
--- back/game/urls.py
+++ back/game/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from . import views
-from . import tests
+from . import atests
 
 urlpatterns = [
     # urls de test vv
-    path('test_player', tests.test_player_model, name='test_player'),
-    path('test_game', tests.test_game_model, name='test_game'),
+    path('test_player', atests.test_player_model, name='test_player'),
+    path('test_game', atests.test_game_model, name='test_game'),
     # path('test_buildings', tests.test_buildings_model, name='test_buildings'),
 
     # vraies urls vv
