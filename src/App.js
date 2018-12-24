96063d110fd9d0e17dbbe5cf520f1f2275b50f4a
Yan-rlathotep <campo.yannis@gmail.com>
2018-12-24 10:32:54 +0100

    Wiped useless junk

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ class App extends Component {
       <MainGrid/>
     );
   }
-
-  resizeActions() {
-    var actionsGrid = document.getElementById("actions");
-    var actionsWidth = window.innerWidth / 10.4 * 0.8;
-    var actionsGap = window.innerWidth / 11 * 0.2;
-    actionsGrid.style.gridTemplateColumns = "repeat(10," + actionsWidth + "px)";
-    actionsGrid.style.gridAutoRows = actionsWidth + "px";
-    actionsGrid.style.gridColumnGap = actionsGap + "px";
-    actionsGrid.style.height = "minmax(" + actionsWidth * 2 + ", auto)";
-  }
 }
 
 export default App;
