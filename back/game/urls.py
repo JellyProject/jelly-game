c2719d1a9de8bd509fe01720c329fdebc30b8fe3
Jérémy <jeremy.lauret@eleves.enpc.fr>
2019-02-02 14:10:23 +0100

    Update API URI

--- back/game/urls.py
+++ back/game/urls.py
@@ -4,26 +4,26 @@ from . import views
 # API endpoints.
 urlpatterns = [
     # SourceTechnology
-    path('api/source-technologies', views.SourceTechnologyList.as_view()),
-    path('api/source-technologies/<slug:version>', views.SourceTechnologyVersionList.as_view()),
-    path('api/source-technologies/<slug:version>/<slug:slug>', views.SourceTechnologyVersionDetail.as_view()),
+    path('source-technologies/', views.SourceTechnologyList.as_view()),
+    path('source-technologies/<slug:version>/', views.SourceTechnologyVersionList.as_view()),
+    path('source-technologies/<slug:version>/<slug:slug>', views.SourceTechnologyVersionDetail.as_view()),
     # SourceBuilding
-    path('api/source-buildings', views.SourceBuildingList.as_view()),
-    path('api/source-buildings/<slug:version>', views.SourceBuildingVersionList.as_view()),
-    path('api/source-buildings/<slug:version>/<slug:slug>', views.SourceBuildingVersionDetail.as_view()),
+    path('source-buildings/', views.SourceBuildingList.as_view()),
+    path('source-buildings/<slug:version>/', views.SourceBuildingVersionList.as_view()),
+    path('source-buildings/<slug:version>/<slug:slug>', views.SourceBuildingVersionDetail.as_view()),
     # Game
-    path('api/games', views.GameList.as_view()),
-    path('api/games/<int:pk>', views.GameDetail.as_view()),
+    path('games/', views.GameList.as_view()),
+    path('games/<int:pk>', views.GameDetail.as_view()),
     # Profile
-    path('api/profiles', views.ProfileList.as_view()),
-    path('api/profiles/<slug:username>', views.ProfileDetail.as_view()),
+    path('profiles/', views.ProfileList.as_view()),
+    path('profiles/<slug:username>', views.ProfileDetail.as_view()),
     # Player
-    path('api/players', views.PlayerList.as_view()),
-    path('api/players/<int:pk>', views.PlayerDetail.as_view()),
+    path('players/', views.PlayerList.as_view()),
+    path('players/<int:pk>', views.PlayerDetail.as_view()),
     # Building
-    path('api/players/<int:player_pk>/buildings', views.BuildingList.as_view()),
-    path('api/players/<int:player_pk>/buildings/<slug:slug>', views.BuildingDetail.as_view()),
+    path('players/<int:player_pk>/buildings/', views.BuildingList.as_view()),
+    path('players/<int:player_pk>/buildings/<slug:slug>', views.BuildingDetail.as_view()),
     # Technology
-    path('api/players/<int:player_pk>/technologies', views.TechnologyList.as_view()),
-    path('api/players/<int:player_pk>/technologies/<slug:slug>', views.TechnologyDetail.as_view()),
+    path('players/<int:player_pk>/technologies/', views.TechnologyList.as_view()),
+    path('players/<int:player_pk>/technologies/<slug:slug>', views.TechnologyDetail.as_view()),
 ]
